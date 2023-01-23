60389e9c71efc3ec057ba6dae063d6a76342f540
fernanda-smendes <fersantos21298@gmail.com>
2023-01-23 15:49:45 -0300

    execução do primeiro exercicio proposto em conteudo

---
+++ fundamentos/secao-03-introducao-javascript/dia-23-introducao-javascript/scripts.js
@@ -0,0 +1,8 @@
+const myName = "Fernanda";
+const birthCity = "Mogi das Cruzes-SP";
+let birthYear = 2030;
+const birthCity = "Videira-SC"
+
+console.log(myName);
+console.log(birthYear);
+console.log(birthCity);
