7738faa512ab416dbaf7d6c025f5992657f8d58d
fernanda-smendes <fersantos21298@gmail.com>
2023-01-24 18:20:53 -0300

    adicionado exemplo if/else conteúdo

--- fundamentos/secao-03-introducao-javascript/dia-23-introducao-javascript/scripts.js
+++ fundamentos/secao-03-introducao-javascript/dia-23-introducao-javascript/scripts.js
@@ -1,8 +1,19 @@
 const myName = "Fernanda";
 const birthCity = "Mogi das Cruzes-SP";
 let birthYear = 2030;
-const birthCity = "Videira-SC"
 
 console.log(myName);
 console.log(birthYear);
 console.log(birthCity);
+
+const nota = 90
+
+ if (nota >= 80) {
+    console.log("Parabéns, você faz parte do grupo das pessoas aprovadas!")
+ }
+ else if (nota < 80 && >= 60) {
+    console.log("Você está na nossa fila de espera")
+ }
+ else if (nota < 60) {
+    console.log("Infelizmente, você reprovou")
+ }
