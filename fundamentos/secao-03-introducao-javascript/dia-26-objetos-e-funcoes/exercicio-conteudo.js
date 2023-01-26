e56330e10fbd5dde34a0935fefee437c2dbb197a
fernanda-smendes <fersantos21298@gmail.com>
2023-01-26 18:44:09 -0300

    Adicionando função de comparação

--- fundamentos/secao-03-introducao-javascript/dia-26-objetos-e-funcoes/exercicio-conteudo.js
+++ fundamentos/secao-03-introducao-javascript/dia-26-objetos-e-funcoes/exercicio-conteudo.js
@@ -2,9 +2,11 @@
 let a = 2;
 let b = 5;
 
+// primeiro passo: Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. 
+
 // adição
 
-function soma (a, b) {
+/* function soma (a, b) {
     return a + b;
 }
 
@@ -39,4 +41,16 @@ function mod (a, b) {
     return a % b;
 }
 
-console.log(mod(a, b));
+console.log(mod(a, b)); */
+
+// segundo passo: Faça um programa que retorne o maior de dois números. Defina, no começo do programa, duas constantes com os valores que serão comparados.
+
+function compar(a, b) {
+    if (a > b) {
+        console.log(a + " é maior que " + b);
+    } else {
+        console.log(b + " é maior que " + a);
+    }
+}
+
+
