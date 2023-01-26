2e7a062b55f27b70f02d9f6b9871111cb9d6423c
fernanda-smendes <fersantos21298@gmail.com>
2023-01-26 18:30:30 -0300

    adicionando funções básicas de soma, sub, div, mult e mod.

---
+++ fundamentos/secao-03-introducao-javascript/dia-26-objetos-e-funcoes/exercicio-conteudo.js
@@ -0,0 +1,42 @@
+
+let a = 2;
+let b = 5;
+
+// adição
+
+function soma (a, b) {
+    return a + b;
+}
+
+console.log(soma(a, b));
+
+
+// Subtração
+
+function sub (a, b) {
+    return a - b;
+}
+
+console.log(sub(a, b));
+
+// Multiplicação
+
+function mult (a, b) {
+    return a * b;
+}
+
+console.log(mult(a, b)); 
+
+// Divisão
+
+function div (a, b) {
+    return a / b;
+}
+
+console.log(div(a, b)); 
+
+function mod (a, b) {
+    return a % b;
+}
+
+console.log(mod(a, b));
