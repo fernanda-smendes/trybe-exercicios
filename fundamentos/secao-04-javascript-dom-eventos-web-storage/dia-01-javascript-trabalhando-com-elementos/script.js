a27a8740101c4b3be76160597c6c04c1f1407a75
fernanda-smendes <fersantos21298@gmail.com>
2023-02-01 16:01:41 -0300

    adicionando pastas e arquivos

---
+++ fundamentos/secao-04-javascript-dom-eventos-web-storage/dia-01-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,9 @@
+console.log(document.getElementById("elementoOndeVoceEsta").parentNode);
+
+console.log(document.getElementById("primeiroFilhoDoFilho"). innerText = "Paçoca, meu cãozinho");
+
+console.log(document.getElementById("pai").firstElementChild);
+
+console.log(document.getElementById("elementoOndeVoceEsta").previousElementSibling);
+
+console.log(document.getElementById("elementoOndeVoceEsta").lastElementChild);
