c49751870afdc6474c185dc1df6c06091e10bd5a
fernanda-smendes <fersantos21298@gmail.com>
2023-03-03 21:26:14 -0300

    Adicionando exercícios propostos em conteúdo

--- fundamentos/secao-08-hofs-js/dia-03-03-hof-sort-map/script.js
+++ fundamentos/secao-08-hofs-js/dia-03-03-hof-sort-map/script.js
@@ -10,3 +10,25 @@ const people = [
      return a.age - b.age
   })
   console.log(people);
+
+  const casa = ['Paçoca', 'Fer', 'Kayque'];
+
+const pessoaCasa = casa.map((elemento) => {
+    const cadastro = {};
+    cadastro.nome = casa;
+    cadastro.local = 'SC';
+    return cadastro
+})
+
+console.log(pessoaCasa);
+
+const products = ['Arroz', 'Feijao', 'Alface', 'Tomate'];
+
+const prices = [2.99, 3.99, 1.5, 2];
+
+const updateProducts = (listProducts, listPrices) => listProducts.map((product, index) => (
+  { [product]: listPrices[index] }
+));
+
+const listProducts = updateProducts(products, prices);
+console.log(listProducts);
