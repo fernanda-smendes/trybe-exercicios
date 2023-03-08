99c9803183588c4930a5350d706ace2c759fd35b
fernanda-smendes <fersantos21298@gmail.com>
2023-03-08 17:58:18 -0300

    Adicionando exercicio 4

---
+++ fundamentos/secao-08-hofs-js/dia-08-03-arraydest-defaultdest-objprop-defaultpar/exercise4.js
@@ -0,0 +1,26 @@
+const student1 = {
+    name: `Claudia`,
+    lastName: `Farias`,
+    age: 23,
+  }
+  
+  const student2 = {
+    name: `Vitor`,
+    age: 20,
+  }
+  
+  // escreva 'getLastName' abaixo para receber os objetos e retornar sua propriedade `lastName`
+  const getLastName = (student) => {
+    const lastName = student.lastName ?? `lastName não preenchido`;
+    return lastName;
+    
+};
+
+    /*
+    const getLastName = (objStudent) => {
+    const { lastName = `lastName não preenchido` } = objStudent;
+    return lastName;
+  }
+  */
+  console.log(getLastName(student1));
+  console.log(getLastName(student2));
