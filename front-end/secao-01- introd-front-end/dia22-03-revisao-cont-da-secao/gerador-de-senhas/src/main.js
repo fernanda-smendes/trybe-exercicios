719f50d37c981259d1e22400a3101f57cc245f4b
fernanda-smendes <fersantos21298@gmail.com>
2023-03-22 17:34:34 -0300

    Finalizando o gerador de senhas seguras

--- front-end/secao-01- introd-front-end/dia22-03-revisao-cont-da-secao/gerador-de-senhas/src/main.js
+++ front-end/secao-01- introd-front-end/dia22-03-revisao-cont-da-secao/gerador-de-senhas/src/main.js
@@ -2,3 +2,12 @@ import { nanoid } from "nanoid";
 
 console.log(nanoid());
 //console.log("hello, world")
+
+const btnGerador = document.querySelector('button');
+const senhaGerada = document.querySelector('h2');
+
+btnGerador.addEventListener('click', () => {
+   // console.log(nanoid());
+   const senhasAleatórias = nanoid();
+   senhaGerada.innerHTML = senhasAleatórias;
+})
